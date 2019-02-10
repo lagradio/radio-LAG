ba6c31d1f3be5f3663a20c038ea7c601d7a8636b
AKostiv8 <nolik2000@yahoo.com>
2019-02-10 21:33:31 +0200

    navigation & routes & page components

--- radiostation/src/app/app-routing.module.ts
+++ radiostation/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TeamComponent } from './components/team/team.component';
+import { NewsComponent } from './components/news/news.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'team', component: TeamComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'programs', component: ProgramsComponent},
+  {path: 'contacts', component: ContactsComponent}
+  // {path: '**', component: PageNotFound}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- radiostation/src/app/app.module.ts
+++ radiostation/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { TeamComponent } from './components/team/team.component';
+import { NewsComponent } from './components/news/news.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    TeamComponent,
+    NewsComponent,
+    ProgramsComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
