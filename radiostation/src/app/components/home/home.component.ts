ad4b5515af2d06ee998734b00da6b1b6608490de
AKostiv8 <nolik2000@yahoo.com>
2019-02-12 23:07:38 +0200

    Main page html/css components (update)

--- radiostation/src/app/components/home/home.component.ts
+++ radiostation/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserModule, DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  backgroundImg;
+  firstImgCard;
+  secondImgCard;
+  thirdImgCard;
+  fourthImgCard;
 
-  constructor() { }
+
+  constructor(
+    private sanitizer:DomSanitizer
+  ) {
+    // this.backgroundImg = '../../../assets/configImages/hero_bg_3.jpg';
+    this.backgroundImg = sanitizer.bypassSecurityTrustStyle('url("../../../assets/configImages/hero_bg_3.jpg")');
+    this.firstImgCard = sanitizer.bypassSecurityTrustStyle('url("../../../assets/configImages/img_1.jpg")');
+    this.secondImgCard = sanitizer.bypassSecurityTrustStyle('url("../../../assets/configImages/img_2.jpg")');
+    this.thirdImgCard = sanitizer.bypassSecurityTrustStyle('url("../../../assets/configImages/img_3.jpg")');
+    this.fourthImgCard = sanitizer.bypassSecurityTrustStyle('url("../../../assets/configImages/img_4.jpg")');
+  }
 
   ngOnInit() {
   }
