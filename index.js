0dca89ffb87f0fb0ef71dd0cdf0a0b2089a050c3
AKostiv8 <nolik2000@yahoo.com>
2019-02-17 01:22:51 +0200

    Test deployment

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ connectedRef.on("value", function(snap) {
 
 
 // Provide static directory for frontend
-app.use(express.static(__dirname + '/radiostation/dist/radiostation/'));
+app.use(express.static(__dirname + '/radiostation/public/'));
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/radiostation/dist/radiostation/index.html'));
+    res.sendFile(path.join(__dirname + '/radiostation/public/index.html'));
 });
 
 // Start Server: Listen on port 8080
