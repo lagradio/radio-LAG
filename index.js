aab67190fbbb459ad3f438b5185162b4c2a30495
lagradio <academic.gymnasium.radio@gmail.com>
2019-03-09 23:11:35 +0200

    conflicts resolved

--- index.js
+++ index.js
@@ -35,14 +35,7 @@ app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 
-// Provide static directory for frontend
-<<<<<<< HEAD
-app.use(express.static(__dirname + '/radiostation/public/'));
 
-// Connect server to Angular 2 Index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/radiostation/public/index.html'));
-=======
 app.use(express.static(__dirname + '/public/'));
 app.use('/authentication', authentication);
 app.use('/cards', cards);
@@ -51,7 +44,6 @@ app.use('/tables', tables);
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
->>>>>>> f0082580172788fdcf1cd5473920f32d3b13da83
 });
 
 // Start Server: Listen on port 8080
